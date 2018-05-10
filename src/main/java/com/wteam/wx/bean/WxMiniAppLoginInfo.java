75902918c158f3a4423a196019043da6ef3c0e94
XQDD <1032453509@qq.com>
2018-05-10 10:32:51 +0800

    初步完成

---
+++ src/main/java/com/wteam/wx/bean/WxMiniAppLoginInfo.java
@@ -0,0 +1,35 @@
+package com.wteam.wx.bean;
+
+public class WxMiniAppUser {
+    //用户唯一标识
+    private String openid;
+    //会话密钥
+    private String session_key;
+    //用户在开放平台的唯一标识符
+    private String unionid;
+
+
+    public String getOpenid() {
+        return openid;
+    }
+
+    public void setOpenid(String openid) {
+        this.openid = openid;
+    }
+
+    public String getSession_key() {
+        return session_key;
+    }
+
+    public void setSession_key(String session_key) {
+        this.session_key = session_key;
+    }
+
+    public String getUnionid() {
+        return unionid;
+    }
+
+    public void setUnionid(String unionid) {
+        this.unionid = unionid;
+    }
+}
---
+++ src/main/java/com/wteam/wx/bean/WxUnionIdUser.java
@@ -0,0 +1,4 @@
+package com.wteam.wx.bean;
+
+public class WxUnionIdUser {
+}
